08788e9558795cb01365a78b16f84f9e30c8f0d3
Patricia G Raftery <praftery8@gmail.com>
2018-04-15 21:36:58 -0700

    tests, views seperated

--- pyramid_stocks/pyramid_stocks/tests/conftest.py
+++ pyramid_stocks/pyramid_stocks/tests/conftest.py
@@ -1,13 +1,84 @@
 import pytest
 from pyramid import testing
+import os
+from ..models.meta import Base
+from ..models import Stock
+from ..models import Account
 
 
 @pytest.fixture
-def dummy_request():
-    return testing.DummyRequest()
+def dummy_request(db_session):
+    """Create a dummy request"""
+    return testing.DummyRequest(dbsession=db_session)
 
 
 @pytest.fixture
-def dummy_get_request():
-    request = testing.DummyRequest()
-    request.method = 'GET'
+def test_stock():
+    """Set up a test stock"""
+    return Stock(
+        symbol="GE",
+        companyName="General Electric Company",
+        exchange="New York Stock Exchange",
+        industry="Industrial Products",
+        website="http://www.ge.com",
+        description="General Electric Co is a digital industrial company. It operates in various segments, including power and water, oil and gas, energy management, aviation, healthcare, transportation, appliances and lighting, and more.",
+        CEO="John L. Flannery",
+        issueType="cs",
+        sector="Industrials"
+    )
+
+
+@pytest.fixture
+def configuration(request):
+    """Setup a database for testing purposes"""
+    config = testing.setUp(settings={
+        'sqlalchemy.url': os.environ['TEST_DATABASE_URL']
+    })
+    config.include('..models')
+    config.include('..routes')
+
+    def teardown():
+        testing.tearDown()
+
+    request.addfinalizer(teardown)
+    return config
+
+
+@pytest.fixture
+def db_session(configuration, request):
+    """Create a database session for interacting with the test database"""
+    SessionFactory = configuration.registry['dbsession_factory']
+    session = SessionFactory()
+    engine = session.bind
+    Base.metadata.create_all(engine)
+
+    def teardown():
+        session.transaction.rollback()
+        Base.metadata.drop_all(engine)
+
+    request.addfinalizer(teardown)
+    return session
+
+
+@pytest.fixture
+def add_stock(dummy_request, test_stock):
+    """Add a stock to database"""
+    dummy_request.dbsession.add(test_stock)
+    return test_stock
+
+
+@pytest.fixture
+def test_user():
+    """Set up a test user"""
+    return Account(
+        username="testtest",
+        password="testpass",
+        email="test@testthis.com",
+    )
+
+
+@pytest.fixture
+def add_user(dummy_request, test_user):
+    """Add a user to database"""
+    dummy_request.dbsession.add(test_user)
+    return test_user
--- pyramid_stocks/pyramid_stocks/views/default.py
+++ pyramid_stocks/pyramid_stocks/views/default.py
@@ -1,149 +1,170 @@
-# from pyramid.response import Response
 from pyramid.view import view_config
-
-# from sqlalchemy.exc import DBAPIError
-
-from pyramid.httpexceptions import HTTPFound, HTTPNotFound, HTTPException
-
-import requests
-import json
-from ..models import Stock
-from . import DB_ERR_MSG
-from sqlalchemy.exc import DBAPIError, IntegrityError
-# from ..sample_data import MOCK_DATA
-API_URL = 'https://api.iextrading.com/1.0'
+from pyramid.security import NO_PERMISSION_REQUIRED
 
 
-@view_config(route_name='home',
+@view_config(
+    route_name='home',
     renderer='../templates/index.jinja2',
-    request_method='GET')
+    request_method='GET',
+    permission=NO_PERMISSION_REQUIRED
+    )
 def home_view(request):
-    return{}
-
-
-@view_config(route_name='portfolio',
-    renderer='../templates/portfolio.jinja2')
-def portfolio_view(request):
-    """portfolio view"""
-    try:
-        query = request.dbsession.query(Stock)
-        all_stocks = query.all()
-    except DBAPIError:
-        return DBAPIError(DB_ERR_MSG, content_type='text/plain', status=500)
-
-    return {'stocks': all_stocks}
-
-
-@view_config(route_name='detail',
-    renderer='../templates/stock-detail.jinja2')
-def detail_view(request):
-    """single stock detail view"""
-    try:
-        symbol = request.matchdict['symbol']
-    except KeyError:
-        return HTTPNotFound()
-
-    try:
-        query = request.dbsession.query(Stock)
-        stock_detail = query.filter(Stock.symbol == symbol).first()
-    except DBAPIError:
-        return DBAPIError(DB_ERR_MSG, content_type='text/plain', status=500)
-
-    return {'stock': stock_detail}
-
-    # for stock in MOCK_DATA:
-    #     if stock['symbol'] == symbol:
-    #         return {'stock': stock}
-
-
-@view_config(route_name='auth',
-    renderer='../templates/auth.jinja2')
-def get_auth_view(request):
-    if request.method == 'GET':
-        try:
-        # take a look at the data in the GET req
-        # access form data
-            username = request.GET['username']
-            password = request.GET['password']
-            print('User: {}, Pass: {}'.format(username, password))
-
-            return HTTPFound(location=request.route_url('portfolio'))
-
-        except KeyError:
-            return {}
-
-    if request.method == 'POST':
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
-        print('User: {}, Pass: {}, Email: {}'.format(username, password, email))
-
-        return HTTPFound(location=request.route_url('portfolio'))
-
-    return HTTPNotFound()  # some exception, probs a 404
-
-
-@view_config(route_name='stock',
-renderer='../templates/stock-add.jinja2')
-def add_view(request):
-    """stock view"""
-    if request.method == 'POST':
-        symbol = request.POST['symbol']
-        response = requests.get('{}/stock/{}/company'.format(API_URL, symbol))
-        company = response.json()
-
-        model = Stock(**company)
-        try:
-            request.dbsession.add(model)
-        except IntegrityError:
-            pass
-
-        return HTTPFound(location=request.route_url('portfolio'))
-
-
-    if request.method == 'GET':
-        try:
-            symbol = request.GET['symbol']
-        except KeyError:
-            return {}
-
-        response = requests.get('{}/stock/{}/company'.format(API_URL, symbol))
-        try:
-            data = response.json()
-            return {'company': data}
-        except json.decoder.JSONDecodeError:
-            return {'err': 'Invalid Symbol'}
-
-    return HTTPNotFound()
+    """home view"""
+    return {}
 
 
 
+# OLD
+# KEEP FOR REFERENCE FOR NEW FILES
 
 
 
+# # from pyramid.response import Response
+# from pyramid.view import view_config
 
-# @view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
-# def my_view(request):
+# # from sqlalchemy.exc import DBAPIError
+
+# from pyramid.httpexceptions import HTTPFound, HTTPNotFound, HTTPException
+
+# import requests
+# import json
+# from ..models import Stock
+# from . import DB_ERR_MSG
+# from sqlalchemy.exc import DBAPIError, IntegrityError
+# # from ..sample_data import MOCK_DATA
+# API_URL = 'https://api.iextrading.com/1.0'
+
+
+# @view_config(route_name='home',
+#     renderer='../templates/index.jinja2',
+#     request_method='GET')
+# def home_view(request):
+#     return{}
+
+
+# @view_config(route_name='portfolio',
+#     renderer='../templates/portfolio.jinja2')
+# def portfolio_view(request):
+#     """portfolio view"""
 #     try:
-#         query = request.dbsession.query(MyModel)
-#         one = query.filter(MyModel.name == 'one').first()
+#         query = request.dbsession.query(Stock)
+#         all_stocks = query.all()
 #     except DBAPIError:
-#         return Response(db_err_msg, content_type='text/plain', status=500)
-#     return {'one': one, 'project': 'pyramid_stocks'}
+#         return DBAPIError(DB_ERR_MSG, content_type='text/plain', status=500)
+
+#     return {'stocks': all_stocks}
 
 
-# db_err_msg = """\
-# Pyramid is having a problem using your SQL database.  The problem
-# might be caused by one of the following things:
+# @view_config(route_name='detail',
+#     renderer='../templates/stock-detail.jinja2')
+# def detail_view(request):
+#     """single stock detail view"""
+#     try:
+#         symbol = request.matchdict['symbol']
+#     except KeyError:
+#         return HTTPNotFound()
 
-# 1.  You may need to run the "initialize_pyramid_stocks_db" script
-#     to initialize your database tables.  Check your virtual
-#     environment's "bin" directory for this script and try to run it.
+#     try:
+#         query = request.dbsession.query(Stock)
+#         stock_detail = query.filter(Stock.symbol == symbol).first()
+#     except DBAPIError:
+#         return DBAPIError(DB_ERR_MSG, content_type='text/plain', status=500)
 
-# 2.  Your database server may not be running.  Check that the
-#     database server referred to by the "sqlalchemy.url" setting in
-#     your "development.ini" file is running.
+#     return {'stock': stock_detail}
 
-# After you fix the problem, please restart the Pyramid application to
-# try it again.
-# """
+#     # for stock in MOCK_DATA:
+#     #     if stock['symbol'] == symbol:
+#     #         return {'stock': stock}
+
+
+# @view_config(route_name='auth',
+#     renderer='../templates/auth.jinja2')
+# def get_auth_view(request):
+#     if request.method == 'GET':
+#         try:
+#         # take a look at the data in the GET req
+#         # access form data
+#             username = request.GET['username']
+#             password = request.GET['password']
+#             print('User: {}, Pass: {}'.format(username, password))
+
+#             return HTTPFound(location=request.route_url('portfolio'))
+
+#         except KeyError:
+#             return {}
+
+#     if request.method == 'POST':
+#         username = request.POST['username']
+#         email = request.POST['email']
+#         password = request.POST['password']
+#         print('User: {}, Pass: {}, Email: {}'.format(username, password, email))
+
+#         return HTTPFound(location=request.route_url('portfolio'))
+
+#     return HTTPNotFound()  # some exception, probs a 404
+
+
+# @view_config(route_name='stock',
+# renderer='../templates/stock-add.jinja2')
+# def add_view(request):
+#     """stock view"""
+#     if request.method == 'POST':
+#         symbol = request.POST['symbol']
+#         response = requests.get('{}/stock/{}/company'.format(API_URL, symbol))
+#         company = response.json()
+
+#         model = Stock(**company)
+#         try:
+#             request.dbsession.add(model)
+#         except IntegrityError:
+#             pass
+
+#         return HTTPFound(location=request.route_url('portfolio'))
+
+
+#     if request.method == 'GET':
+#         try:
+#             symbol = request.GET['symbol']
+#         except KeyError:
+#             return {}
+
+#         response = requests.get('{}/stock/{}/company'.format(API_URL, symbol))
+#         try:
+#             data = response.json()
+#             return {'company': data}
+#         except json.decoder.JSONDecodeError:
+#             return {'err': 'Invalid Symbol'}
+
+#     return HTTPNotFound()
+
+
+
+
+
+
+
+# # @view_config(route_name='home', renderer='../templates/mytemplate.jinja2')
+# # def my_view(request):
+# #     try:
+# #         query = request.dbsession.query(MyModel)
+# #         one = query.filter(MyModel.name == 'one').first()
+# #     except DBAPIError:
+# #         return Response(db_err_msg, content_type='text/plain', status=500)
+# #     return {'one': one, 'project': 'pyramid_stocks'}
+
+
+# # db_err_msg = """\
+# # Pyramid is having a problem using your SQL database.  The problem
+# # might be caused by one of the following things:
+
+# # 1.  You may need to run the "initialize_pyramid_stocks_db" script
+# #     to initialize your database tables.  Check your virtual
+# #     environment's "bin" directory for this script and try to run it.
+
+# # 2.  Your database server may not be running.  Check that the
+# #     database server referred to by the "sqlalchemy.url" setting in
+# #     your "development.ini" file is running.
+
+# # After you fix the problem, please restart the Pyramid application to
+# # try it again.
+# # """
